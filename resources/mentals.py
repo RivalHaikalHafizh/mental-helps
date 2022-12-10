b76c4d5440db8a007aab66a7f0819c4869058f67
RivalHaikalHafizh <rivalhh130702@gmail.com>
2022-12-10 08:42:32 +0700

    merubah library encoder

--- resources/mentals.py
+++ resources/mentals.py
@@ -2,7 +2,6 @@ from flask import jsonify, Blueprint, abort,make_response
 from flask_restful import Resource, Api, reqparse, fields, marshal, marshal_with
 import json
 from flask_cors import CORS,cross_origin
-from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
 from flask_jwt_extended import JWTManager, jwt_required,create_access_token, get_jwt_identity
@@ -14,18 +13,14 @@ mental_fields = {
     'Educational_level':fields.String,
     'Screening_time':fields.String,
     'lack_of_practical_exposure':fields.String,
-    # 'Irregular_eating_habits':fields.String,
     'Exercise':fields.String,
     'depressiveness':fields.String,
     'unnecessary_misunderstandings':fields.String,
     'online_courses':fields.String,
-    # 'procrastination':fields.Integer,
     'overthinking':fields.Integer,
     'social_media_hours':fields.Integer,
     'hobby_hours':fields.Integer,
     'increased_sleep_hours':fields.Integer,
-    # 'online_difficulty_level':fields.Integer,
-    # 'focus_level':fields.Integer,
     'health_problems': fields.String
 }
 
@@ -53,13 +48,6 @@ class UserBase(Resource):
             location=['json'],
 
         )
-        # self.reqparse.add_argument(
-        #     'lack_of_practical_exposure',
-        #     required=True,
-        #     help='lack_of_practical_exposure wajib ada',
-        #     location=['json'],
-
-        # )
         self.reqparse.add_argument(
             'Irregular_eating_habits',
             required=True,
@@ -95,13 +83,6 @@ class UserBase(Resource):
             location=['json'],
 
         )
-        # self.reqparse.add_argument(
-        #     'procrastination',
-        #     required=True,
-        #     help='procrastination wajib ada',
-        #     location=['json'],
-
-        # )
         self.reqparse.add_argument(
             'overthinking',
             required=True,
@@ -130,20 +111,6 @@ class UserBase(Resource):
             location=['json'],
 
         )
-        # self.reqparse.add_argument(
-        #     'online_difficulty_level',
-        #     required=True,
-        #     help='online_difficulty_level wajib ada',
-        #     location=['json'],
-
-        # )
-        # self.reqparse.add_argument(
-        #     'focus_level',
-        #     required=True,
-        #     help='focus_level wajib ada',
-        #     location=['json'],
-
-        # )
         super().__init__()
 
 
@@ -179,21 +146,11 @@ class Mental(UserBase):
             }
         pr = pd.DataFrame(d, index=[0])
         #encoder
-        pr['Age']=pr['Age'].map({'16 to 18 years': 1,'18 to 22 years': 2,'13 to 15 years': 0,'23 to 26 years': 3})
-        pr['Educational_level']= pr['Educational_level'].map({'Higher Secondary School': 0,'Under Graduate': 3,'Secondary School': 2,'Post Graduate': 1})
-        pr['Screening_time']=pr['Screening_time'].map( {'2 to 3 hours': 0,'4 to 5 hours': 2,'3 to 4 hours': 1,'5 to 6 hours': 3})
-        pr['Irregular_eating_habits']=pr['Irregular_eating_habits'].map({'Yes': 2, 'No': 1, 'May be': 0})
-        pr['Exercise']=pr['Exercise'].map({'Rarely': 2, 'Sometimes': 3, 'Never': 1, 'Always': 0})
-        pr['depressiveness']=pr['depressiveness'].map({'Occasionally': 2,'Never': 1,'All the time': 0,'Rarely': 3})
-        pr['overthinking']=pr['overthinking'].map({'Yes': 2, 'No': 1, 'May be': 0})
-        pr['unnecessary_misunderstandings']=pr['unnecessary_misunderstandings'].map({'No': 1, 'Yes': 2, 'Maybe': 0})
-        pr['online_courses']=pr['online_courses'].map({'1': 1, '0': 0, 'more than 3': 4, '2': 2, '3': 3})
-        # categorical_features=['Age', 'Educational_level', 'Screening_time', 'Irregular_eating_habits', 'Exercise', 'depressiveness','unnecessary_misunderstandings','online_courses']
-        # label_encoders = {}
-        # enc = load('../mental-helps/encoder.joblib') 
-        # for col in pr[categorical_features]:
-        #     pr.replace(enc[col], inplace=True)
-        #encode=pr.to_dict('records')
+        categorical_features=['Age', 'Educational_level', 'Screening_time', 'Irregular_eating_habits', 'Exercise', 'depressiveness','overthinking','unnecessary_misunderstandings','online_courses']
+        label_encoders = {}
+        enc = load('../mental-helps/encoder.joblib') 
+        df_deploy_encoded = enc.transform(pr[categorical_features])
+        pr[categorical_features]=df_deploy_encoded
         #scaler
         same_standard_scaler = load('../mental-helps/scaler.joblib') 
         numerical_features = ['social_media_hours', 'hobby_hours','increased_sleep_hours']
